04986e3d763f5ddfef38f61997269150dd60b312
Travis CI <travis@travis-ci.org>
2018-05-04 10:38:16 +0000

    Deploy code docs to GitHub Pages Travis build: 21
    Commit: ab8a09cc7869e55188abe1a003fb5b672a363958

--- html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['queue_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fsmfsw_2fqueue_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fsmfsw_2fqueue_29',['Queue [![Build Status](https://travis-ci.org/SMFSW/Queue.svg?branch=master)](https://travis-ci.org/SMFSW/Queue)',['../md__r_e_a_d_m_e.html',1,'']]]
-];
--- html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['releasenotes',['ReleaseNotes',['../md__release_notes.html',1,'']]]
-];
