c9b6fabd4017922d84c468527b942a28942a729a
Travis CI <travis@travis-ci.org>
2018-05-21 20:34:24 +0000

    Deploy code docs to GitHub Pages Travis build: 24
    Commit: 70ee2d3c786703f2446b5f695d3da6b6bb807b91

--- html/search/all_c.js
+++ html/search/all_c.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['setup',['setup',['../_lib_tst_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;LibTst.ino'],['../_rollover_test_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;RolloverTest.ino'],['../_simple_queue_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;SimpleQueue.ino']]],
   ['simplequeue_2eino',['SimpleQueue.ino',['../_simple_queue_8ino.html',1,'']]],
+  ['sizeof',['sizeOf',['../class_queue.html#af064936c416797c9200adc19286cef2b',1,'Queue']]],
   ['strrec',['strRec',['../structstr_rec.html',1,'']]]
 ];
--- html/search/functions_9.js
+++ html/search/functions_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['setup',['setup',['../_lib_tst_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;LibTst.ino'],['../_rollover_test_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;RolloverTest.ino'],['../_simple_queue_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;SimpleQueue.ino']]]
+  ['setup',['setup',['../_lib_tst_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;LibTst.ino'],['../_rollover_test_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;RolloverTest.ino'],['../_simple_queue_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;SimpleQueue.ino']]],
+  ['sizeof',['sizeOf',['../class_queue.html#af064936c416797c9200adc19286cef2b',1,'Queue']]]
 ];
